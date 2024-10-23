06e20248723af86f579c4abb193e31262a2d4caf
Mr.D19X <ziaulkamal1109@gmail.com>
2024-10-23 17:29:40 +0700

    update

--- install.sh
+++ install.sh
@@ -14,13 +14,29 @@ check_distro() {
 install_ubuntu() {
     echo "Menginstal dependensi untuk Ubuntu..."
     sudo apt-get update
-    sudo apt-get install -y python3-pyqt5 libsecp256k1-dev python3-cryptography
+    sudo apt-get install -y python3-pyqt5 libsecp256k1-dev python3-cryptography python3-setuptools python3-pip
 }
 
 # Fungsi untuk menginstal dependensi pada CentOS
 install_centos() {
     echo "Menginstal dependensi untuk CentOS..."
-    sudo yum install -y python3-qt5 libsecp256k1-devel python3-cryptography
+    sudo yum install -y python3-qt5 libsecp256k1-devel python3-cryptography python3-setuptools python3-pip
+}
+
+# Fungsi untuk mengunduh dan menginstal Electrum
+install_electrum() {
+    echo "Mengunduh Electrum..."
+    wget https://download.electrum.org/4.5.7/Electrum-4.5.7.tar.gz
+    wget https://download.electrum.org/4.5.7/Electrum-4.5.7.tar.gz.asc
+    echo "Memverifikasi file..."
+    gpg --verify Electrum-4.5.7.tar.gz.asc
+
+    echo "Membuat lingkungan virtual..."
+    python3 -m venv env
+    source env/bin/activate
+
+    echo "Menginstal Electrum..."
+    pip install Electrum-4.5.7.tar.gz
 }
 
 # Memeriksa distro
@@ -39,5 +55,8 @@ case "$distro" in
         exit 1
         ;;
 esac
+
+# Menginstal Electrum
+install_electrum
 
 echo "Instalasi selesai."
